aa654618ae0a29cf89acb80d450250324955d4b3
Chamelee <chxxxlxx@gmail.com>
2023-02-20 18:46:39 +0530

    Add UI Components to Homepage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -12,7 +12,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/back.png"), fit: BoxFit.fill),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Contact Us",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff2E4450),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Text(
+                    "About Us",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff2E4450),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 55,
+                  ),
+                ],
+              ),
+              Expanded(child: SizedBox()),
+              Image.asset("assets/logo.png", width: 300),
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset("assets/home_img.png", width: 300),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
